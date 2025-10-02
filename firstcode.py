a125bbfec33b65cd184f0674c184abbfb091dfd1
Noble20Boadi03 <143855390+Noble20Boadi03@users.noreply.github.com>
2025-10-02 13:37:51 +0000

    CRUD

---
+++ firstcode.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from models import Products
+
+app = FastAPI()
+
+products=[
+  Products(id=1, name="jack n' jill",price=50),
+  Products(id=2, name="oreos", price=60),
+  Products(id=3, name="potato", price=70)
+]
+
+@app.get('/get_all_products')
+def get_Products():
+  return products
+
+@app.get('/get_by_id/{id}')
+def get_product(id:int):
+  for p in products:
+    if p.id == id:
+      return p
+    
+@app.delete('/delete_by_id/{id}')
+def del_by_id(id:int):
+  for i in range(len(products)):
+    if products[i].id == id:
+      del products[i]
+  return products
+
+@app.post('/add')
+def add_prod(p:Products):
+  for prod in products:
+    if prod.id == p.id:
+      return('Already exist')
+  products.append(p)
+  return products
+
+@app.put('/update/{id}')
+def update_prod(id:int, prod:Products):
+  for i in range(len(products)):
+    if products[i].id == id:
+      products[i] = prod
+  return products
+
