f9fe217596925e3d70092bc41a54f404c95e8b96
Noble20Boadi03 <143855390+Noble20Boadi03@users.noreply.github.com>
2025-10-04 19:00:58 +0000

    Finishing touches

--- firstcode.py
+++ firstcode.py
@@ -1,53 +1,68 @@
 from fastapi import FastAPI, Depends
-from models import Products
+from models import Product
+from fastapi.middleware.cors import CORSMiddleware
 from postgre_database import session, engine
 import postgre_database_models
 from sqlalchemy.orm import Session
 
 app = FastAPI()
 
+#for the frontend to merge with the backend
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["http://localhost:3000"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
+#creates the tables using the base model
 postgre_database_models.Base.metadata.create_all(bind=engine)
 
 
+#products that is read and displayed
 products=[
-  Products(id=1, name="jack n' jill",price=50),
-  Products(id=2, name="oreos", price=60),
-  Products(id=3, name="potato", price=70),
-  Products(id=4, name="flakes", price=70)
+  Product(id=1, name="jack n' jill", description='vanilla ', price=50, quantity=4),
+  Product(id=2, name="oreos", description='choco', price=60, quantity=4),
+  Product(id=3, name="potato", description='vanilla fruit', price=70, quantity=4),
+  Product(id=4, name="flakes", description='vanilla choco', price=70, quantity=4)
 ]
 
+# create a session function for new db sessions
 def get_db():
   db = session()
   try:
     yield db
   finally:
-    db.commit
+    db.close()
 
+#function that reads the products list and insert it into the db
 def db_init():
   db = session()
   for p in products:
-    db.merge(postgre_database_models.Products(**p.model_dump()))
+    db.merge(postgre_database_models.Product(**p.model_dump())) #converts p to dict and then to key and value pair for Product
 
   db.commit()
 
 db_init()
 
 #reads db and display through url
-@app.get('/get_all_products')
+@app.get('/products')
 def get_Products(db: Session = Depends(get_db)):
-  all_Products = db.query(postgre_database_models.Products).all()
+  all_Products = db.query(postgre_database_models.Product).all()
   return all_Products
 
-@app.get('/get_by_id/{id}')
+#get product by query db to find matching id
+@app.get('/products/{id}')
 def get_product(id:int, db: Session =Depends(get_db)):
-  prod = db.query(postgre_database_models.Products).filter(postgre_database_models.Products.id == id).first()
+  prod = db.query(postgre_database_models.Product).filter(postgre_database_models.Product.id == id).first()
   if prod:
     return prod
   return "product not found"
     
-@app.delete('/delete_by_id/{id}')
+@app.delete('/products/{id}')
 def del_by_id(id:int, db: Session = Depends(get_db)):
-  prod = db.query(postgre_database_models.Products).filter(postgre_database_models.Products.id == id).first()
+  prod = db.query(postgre_database_models.Product).filter(postgre_database_models.Product.id == id).first()
   if prod:
     db.delete(prod)
     db.commit()
@@ -59,9 +74,9 @@ def del_by_id(id:int, db: Session = Depends(get_db)):
   #     del products[i]
   # return products
 
-@app.post('/add')
-def add_prod(p:Products, db:Session = Depends(get_db)):
-  db.add(postgre_database_models.Products(**p.model_dump()))
+@app.post('/products')
+def add_prod(p:Product, db:Session = Depends(get_db)):
+  db.add(postgre_database_models.Product(**p.model_dump()))
   db.commit()
   return p
 
@@ -71,12 +86,14 @@ def add_prod(p:Products, db:Session = Depends(get_db)):
   # products.append(p)
   # return products
 
-@app.put('/update/{id}')
-def update_prod(id:int, prod:Products, db:Session = Depends(get_db)):
-  p = db.query(postgre_database_models.Products).filter(postgre_database_models.Products.id == id).first()
+@app.put('/products')
+def update_prod(id:int, prod:Product, db:Session = Depends(get_db)):
+  p = db.query(postgre_database_models.Product).filter(postgre_database_models.Product.id == id).first()
   if p:
     p.name = prod.name
+    p.description = prod.description
     p.price = prod.price
+    p.quantity = prod.quantity
     db.commit()
     return "prod updated"
   else:
--- postgre_database_models.py
+++ postgre_database_models.py
@@ -3,9 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class Products(Base):
+class Product(Base):
   __tablename__ = "product"
 
-  id = Column(Integer, primary_key=True)
+  id = Column(Integer, primary_key=True, index=True)
   name = Column(String)
+  description = Column(String)
   price = Column(Float)
+  quantity = Column(Integer)
